5fa96c72b349f51f460123b20ed3f4c4539ba4cd
daichi_sato <daichiexperience@gmail.com>
2022-12-20 17:37:22 +0900

    [add] query

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -8,16 +8,25 @@ import { Resolvers } from '../../graphql/dist/generated-server';
 const path = join(process.cwd(), 'graphql', 'schema.graphql');
 const typeDefs = readFileSync(path).toString('utf-8');
 
-const users = [
-  { id: '1', name: 'Alice' },
-  { id: '2', name: 'Bob' },
-  { id: '3', name: 'Carol' },
+type Team = 'Red' | 'White';
+
+const teams: { id: string; name: Team }[] = [
+  { id: '1', name: 'Red' },
+  { id: '2', name: 'White' },
 ];
 
-// スキーマと実際のデータ構造の紐付けを resolvers で行う
+type User = { id: string; name: string; teamName: Team };
+
+const users: User[] = [
+  { id: '1', name: 'Alice', teamName: 'Red' },
+  { id: '2', name: 'Bob', teamName: 'Red' },
+  { id: '3', name: 'Carol', teamName: 'White' },
+];
+
 const resolvers: Resolvers = {
   Query: {
     users: () => users,
+    teams: () => teams,
   },
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,33 +4,47 @@ import {
   ApolloProvider,
   useQuery,
 } from '@apollo/client';
-import { GetUsersNameDocument } from '../graphql/dist/generated-client';
+import { GetUsersAndTeamsDocument } from '../graphql/dist/generated-client';
 
 const client = new ApolloClient({
   uri: 'http://localhost:3000/api/graphql',
   cache: new InMemoryCache(),
 });
 
-function Users() {
-  const { loading, error, data } = useQuery(GetUsersNameDocument);
+function UsersAndTeams() {
+  const { loading, error, data } = useQuery(GetUsersAndTeamsDocument);
 
   if (loading) return <p>Loading...</p>;
   if (error || !data) return <p>Error</p>;
 
+  const { users, teams } = data;
+
   return (
-    <ul>
-      {data.users.map((user) => {
-        console.log(Object.getOwnPropertyNames(user)); // ['__typename', 'name']
-        return <li key={user.id}>{user.name}</li>;
-      })}
-    </ul>
+    <>
+      <h1>Team List</h1>
+      <ul>
+        {teams.map(({ id, name }) => {
+          return <li key={id}>{name}</li>;
+        })}
+      </ul>
+      <h1>User List</h1>
+      <ul>
+        {users.map(({ id, name, teamName }) => {
+          return (
+            <li key={id}>
+              <b>{name}</b> belong {teamName} team
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
 
 export default function App() {
   return (
     <ApolloProvider client={client}>
-      <Users />
+      <UsersAndTeams />
     </ApolloProvider>
   );
 }

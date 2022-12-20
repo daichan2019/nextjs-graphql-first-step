073eceec87b9aa028d365fdc441880d6e3ea1789
daichi_sato <daichiexperience@gmail.com>
2022-12-20 16:39:22 +0900

    [add] resolver

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -8,8 +8,18 @@ import { Resolvers } from '../../graphql/dist/generated-server';
 const path = join(process.cwd(), 'graphql', 'schema.graphql');
 const typeDefs = readFileSync(path).toString('utf-8');
 
+const users = [
+  { id: '1', name: 'Alice' },
+  { id: '2', name: 'Bob' },
+  { id: '3', name: 'Carol' },
+];
+
 // スキーマと実際のデータ構造の紐付けを resolvers で行う
-const resolvers: Resolvers = {};
+const resolvers: Resolvers = {
+  Query: {
+    users: () => users,
+  },
+};
 
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
